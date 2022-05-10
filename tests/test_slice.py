87aa74bb9887cc1b74e1f115ea12125d3333fc5b
David Venzke <digerian@protonmail.com>
2022-05-10 16:17:52 +0200

    udacity starter code, pytest setup for github actions

---
+++ tests/test_slice.py
@@ -0,0 +1,20 @@
+import pandas as pd
+import pytest
+
+
+@pytest.fixture
+def data():
+    """ Simple function to generate some fake Pandas data."""
+    df = pd.DataFrame(
+        {
+            "id": [1, 2, 3],
+            "numeric_feat": [3.14, 2.72, 1.62],
+            "categorical_feat": ["dog", "dog", "cat"],
+        }
+    )
+    return df
+
+
+def test_data_shape(data):
+    """ If your data is assumed to have no null values then this is a valid test. """
+    assert data.shape == data.dropna().shape, "Dropping null changes shape."
