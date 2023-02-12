671dcac74d1b4ed54d74046875737e3e8ecb922c
David Venzke <digerian@protonmail.com>
2023-02-12 18:35:56 +0100

    writing unittests with pytest

--- tests/test_slice.py
+++ tests/test_slice.py
@@ -5,20 +5,13 @@ import pytest
 @pytest.fixture
 def data():
     """ Simple function to generate some fake Pandas data."""
-    df = pd.DataFrame(
-        {
-            "id": [1, 2, 3],
-            "numeric_feat": [3.14, 2.72, 1.62],
-            "categorical_feat": ["dog", "dog", "cat"],
-        }
-    )
-    return df
+
+    data = pd.read_csv('data/census.csv')
+    data.columns = data.columns.str.strip()
+    return data
 
 
 def test_data_shape(data):
-    """ If your data is assumed to have no null values then this is a valid test. """
-    assert data.shape == data.dropna().shape, "Dropping null changes shape."
+    """ Tests if our data has all 107 features and 1 target variable """
+    assert data.shape[1]== 108, "Data does not have the expected shape."
 
-def test_data_shape_2(data):
-    """ If your data is assumed to have no null values then this is a valid test. """
-    assert data.shape == data.dropna().shape, "Dropping null changes shape."
